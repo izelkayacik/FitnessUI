8cf291ce6d1b93c94dfb1118cdec1f8603d708a4
izelkayacik <izelkayacik@hotmail.com>
2020-06-05 19:43:35 +0300

    first commit

---
+++ app/src/main/java/com/izelkayacik/fitnessui/MainActivity.java
@@ -0,0 +1,31 @@
+package com.izelkayacik.fitnessui;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.izelkayacik.fitnessui.R;
+import com.izelkayacik.fitnessui.fragment.ViewPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    ViewPager viewPager;
+    TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.main_activity_view_pager);
+        tabLayout = findViewById(R.id.main_activity_tab_layout);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
